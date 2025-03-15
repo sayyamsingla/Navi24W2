75216c417e7aa6bb5831e9e2450dca1f165d9648
sayyamsingla <singlasayyam@gmail.com>
2025-03-15 13:26:22 -0700

    Update robot_obstacles.py

--- arm/continuous_env/continuous_env/robot_obstacles.py
+++ arm/continuous_env/continuous_env/robot_obstacles.py
@@ -14,12 +14,13 @@ from numpy import ndarray
 from numpy.typing import NDArray
 from pygame import gfxdraw
 from pygame.surface import Surface
-from continuous_env import policy
 
 from continuous_env.robot_dynamics import Robot
 from util.astar_search import astar_pathfinding
 from util.maze_generator import maze_generator
 from util.maze_helpers import find_unique_item
+
+from continuous_env.policy import random_move
 
 """
 Car racing environment adapted with maze generation and A*, originally from
@@ -97,13 +98,7 @@ class RobotObstacles(gym.Env[NDArray[np.uint8], NDArray[np.float32]]):  # type: 
         self,
         render_mode: Optional[str] = None,
         verbose: bool = False,
-        seed: Optional[int] = None,
     ) -> None:
-
-        if seed is None:
-            self.seed = np.random.default_rng(seed)
-        else:
-            self.seed = np.random.default_rng(seed)
         self.render_mode: Optional[str] = render_mode
         self.verbose: bool = verbose
         self.reward: float = 0.0
@@ -517,9 +512,6 @@ class RobotObstacles(gym.Env[NDArray[np.uint8], NDArray[np.float32]]):  # type: 
             self.isopen = False
             pygame.quit()
 
-    def get_internal_state(self) -> list[list[int]] | None:
-        return self.maze
-
 
 def main() -> None:
     a = np.array([0.0, 0.0, 0.0, 0.0])
@@ -568,11 +560,11 @@ def main() -> None:
         total_reward = 0.0
         steps = 0
         restart = False
+        s = None
+        r = None
         while True:
-            s = env.get_internal_state()
-            a = policy.random_move(s)
-            new_pixel_values, r, terminated, truncated, info = env.step(a)
-            s_prime = env.get_internal_state()
+            a = random_move(s, s_prime = None, reward=r)
+            s, r, terminated, truncated, info = env.step(a)
             total_reward += r
             if steps % 200 == 0 or terminated or truncated:
                 print("\naction " + str([f"{x:+0.2f}" for x in a]))
@@ -586,7 +578,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-    # cProfile.run("main()", "profile_output")
-
-    # stats = pstats.Stats("profile_output")
-    # stats.sort_stats("tottime").print_stats()
+   
